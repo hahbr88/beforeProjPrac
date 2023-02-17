40c0092ccf92f76cfafafcac51f0144d4cc97146
hahbr88 <hahbr88@hanmail.net>
2023-02-17 19:00:28 +0900

    연습용 초기설정
    - 스크롤 감지 및 애니매이션 제작 예정

---
+++ src/components/layouts/MainLayout.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+interface IProps {
+  children: React.ReactElement;
+}
+
+const MainLayout = () => {
+  return (
+    <div className="tb2 flex vh100">
+      <div></div>
+    </div>
+  );
+};
+
+export default MainLayout;
---
+++ src/pages/main/Main.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainLayout from "../../components/layouts/MainLayout";
+import "./index.css";
+const Main = () => {
+  return (
+    <div className="mainpage-wrap border-box tb1">
+      <MainLayout />
+    </div>
+  );
+};
+
+export default Main;
